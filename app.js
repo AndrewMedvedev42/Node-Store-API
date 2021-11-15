24fa1bc377d27c4113f094f3d2cb2b92b5b38c66
andrewNEW42 <sanjino56@gmail.com>
2021-11-15 12:32:08 +0200

    deploy of files

---
+++ app.js
@@ -0,0 +1,32 @@
+require("dotenv").config()
+require("express-async-errors")
+const port = 5000
+const express = require('express')
+const app = express()
+
+const connectDB = require('./db/connect')
+const productsRouter = require('./routes/products')
+
+const notFoundMiddleware = require('./middleware/not-found')
+const errorMiddleware = require('./middleware/error-handler')
+
+app.use(express.json())
+
+app.get('/',(req, res)=>{
+    res.send('<h1>Hello World</h1>')
+})
+
+app.use('/api/v1/products', productsRouter)
+
+app.use(notFoundMiddleware)
+app.use(errorMiddleware)
+
+const start = async () => {
+    try {
+        await connectDB(process.env.DATABASE_URI)
+        app.listen(port, console.log(`Listening port ${port}`))
+    } catch (error) {
+        console.log(error);
+    }
+}
+start()
